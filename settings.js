a25f62d7dcde21671eb39234df66a862f3cda35f
lakshan01 <110474040+lakshan01@users.noreply.github.com>
2022-09-30 10:25:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
-global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/WhatsApp%20Image%202022-09-30%20at%209.30.23%20AM.jpeg") //gif and video menu
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
